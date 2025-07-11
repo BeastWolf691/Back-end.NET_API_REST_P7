6cfc192c6c39811a5173da94d71b00d8491a2bff
BeastWolf691 <sriver@hotmail.fr>
2025-07-11 18:51:34 +0200

    Update Bid

--- P7CreateRestApi/Domain/BidList.cs
+++ P7CreateRestApi/Domain/BidList.cs
@@ -9,20 +9,20 @@ namespace Dot.Net.WebApi.Domain
         public double? AskQuantity { get; set; }
         public double? Bid { get; set; }
         public double? Ask { get; set; }
-        public string Benchmark { get; set; } = string.Empty;
+        public string? Benchmark { get; set; }
         public DateTime? BidListDate { get; set; }
-        public string Commentary { get; set; } = string.Empty;
-        public string BidSecurity { get; set; } = string.Empty;
-        public string BidStatus { get; set; } = string.Empty;
-        public string Trader { get; set; } = string.Empty;
-        public string Book { get; set; } = string.Empty;
-        public string CreationName { get; set; } = string.Empty;
+        public string? Commentary { get; set; }
+        public string? BidSecurity { get; set; }
+        public string? BidStatus { get; set; }
+        public string? Trader { get; set; }
+        public string? Book { get; set; }
+        public string? CreationName { get; set; }
         public DateTime? CreationDate { get; set; }
-        public string RevisionName { get; set; } = string.Empty;
+        public string? RevisionName { get; set; }
         public DateTime? RevisionDate { get; set; }
-        public string DealName { get; set; } = string.Empty;
-        public string DealType { get; set; } = string.Empty;
-        public string SourceListId { get; set; } = string.Empty;
-        public string Side { get; set; } = string.Empty;
+        public string? DealName { get; set; }
+        public string? DealType { get; set; }
+        public string? SourceListId { get; set; }
+        public string? Side { get; set; }
     }
 }
